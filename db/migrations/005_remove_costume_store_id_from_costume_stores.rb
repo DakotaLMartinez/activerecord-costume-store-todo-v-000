e13b06770ee652347ff4827cebcdbf079cc13afe
Dakota Martinez <dakotaleemusic@gmail.com>
2018-06-28 16:02:19 +0000

    automatically backed up by learn

--- db/migrations/005_remove_costume_store_id_from_costume_stores.rb
+++ db/migrations/005_remove_costume_store_id_from_costume_stores.rb
@@ -1,5 +1,5 @@
-class AddCostumeStoreIdToCostumeStores < ActiveRecord::Migration[4.2]
+class RemoveCostumeStoreIdFromCostumeStores < ActiveRecord::Migration[4.2]
   def change
-    add_column :costume_stores, :costume_store_id, :integer
+    remove_column :costume_stores, :costume_store_id
   end
 end
