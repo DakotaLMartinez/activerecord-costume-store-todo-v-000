bf02ecf106dee75a3fe08c036ee5a567895accda
Dakota Martinez <dakotaleemusic@gmail.com>
2018-06-28 16:05:59 +0000

    automatically backed up by learn

--- db/migrations/006_create_costume_store_costumes.rb
+++ db/migrations/006_create_costume_store_costumes.rb
@@ -3,6 +3,7 @@ class CreateCostumeStoreCostumes < ActiveRecord::Migration[4.2]
     create_table :costume_store_costumes do |t|
       t.integer :costume_store_id
       t.integer :costume_id
+      t.integer :inventory
     end
   end
 end
