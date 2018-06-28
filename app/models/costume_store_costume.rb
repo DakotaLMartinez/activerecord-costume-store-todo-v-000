8a5c4695cd38851e25ee413070f59f069531cff6
Dakota Martinez <dakotaleemusic@gmail.com>
2018-06-28 16:13:26 +0000

    automatically backed up by learn

--- app/models/costume_store_costume.rb
+++ app/models/costume_store_costume.rb
@@ -1,3 +1,20 @@
-class CostumeStoreCostume < ActiveRecord::Base 
+class CostumeStoreCostume < ActiveRecord::Base
+  belongs_to :costume_store
+  belongs_to :costume
 
+  # def costume_store
+  #   CostumeStore.find_by(id: self.costume_store_id)
+  # end
+  #
+  # def costume_store=(costume_store)
+  #   self.costume_store = costume_store
+  # end
+  #
+  # def costume_store_id
+  #   self.costume_store.id
+  # end
+  #
+  # def costume_store_id=(costume_store_id)
+  #   self.costume_store_id = costume_store_id
+  # end
 end
